759df42c7ffc68e9a10f49588801ef3a67c5676e
Kjetil Svergja <kjetil@svergja.com>
2021-02-19 18:33:30 +0100

    added route for kanto

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 
 
 
@@ -10,17 +9,6 @@ export default function Header(props) {
     <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
       <Navbar.Brand as={Link} to="/">PokeWatch</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto ml-2">
-          <Nav.Link>
-            About
-          </Nav.Link>
-        </Nav>
-        <Nav className="mr-sm-2">
-          <Nav.Link key="1" as={Link} to="/login">Login</Nav.Link>
-          <Nav.Link key="2" as={Link} to="/signup">Signup</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
     </Navbar>
 
   )
--- frontend/src/components/pages/home/home.js
+++ frontend/src/components/pages/home/home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Accordion, Button, Form, InputGroup } from 'react-bootstrap';
+import { Accordion, Button, Col, Form, InputGroup, Row, Table } from 'react-bootstrap';
 import Countdown from 'react-countdown';
+import kanto_clean from '../../../assets/img/kanto_clean.png';
 export default function Home() {
     const newRun = 'New run'
     const endRun = 'End run'
@@ -8,7 +9,15 @@ export default function Home() {
     const timerReady = 'Go do it ape!'
     const runStarted = 'run started, end run to start timer'
     const countdownTime = 60000 * 60 * 18 //18 hours in millis
-
+    const [loading, setLoading] = useState(true)
+    const kantoRoute = {
+        Route: [
+            'Vermillion', 'Fuchsia', 'Cinnabar Island', 'Pewter City', 'Cerulean City', 'Celadon City', 'Saffron City'
+        ],
+        Type: [
+            'Electric', 'Poison', 'Fire', 'Rock', 'Water', 'Grass', 'Psychic'
+        ]
+    }
     const [testSaving, setTestSaving] = useState(() =>
         JSON.parse(localStorage.getItem('POKEWATCH-timers')) ||
         {
@@ -66,7 +75,7 @@ export default function Home() {
         let allRegions = { ...regions };
 
         names.forEach(regionName => {
-            
+
             if (testSaving[regionName].timestamp == null || testSaving[regionName].timestamp < Date.now()) return
             allRegions[regionName].timer = dailyResetTimer(regionName, allRegions[regionName].event, testSaving[regionName].timestamp)
             allRegions[regionName].btnClassNames = 'btn-danger'
@@ -74,7 +83,7 @@ export default function Home() {
             allRegions[regionName].event = '1337'
         })
         setRegions(allRegions)
-
+        setLoading(false)
     }, [])
     useEffect(() => {
         saveCountdownToLocalstorage()
@@ -90,7 +99,7 @@ export default function Home() {
 
     const resetTimer = (regionName, ievent) => {
         console.log('reset')
-        setTestSaving((prevstate) => ({...prevstate, [regionName]: { ...prevstate[regionName], timestamp: null } }))
+        setTestSaving((prevstate) => ({ ...prevstate, [regionName]: { ...prevstate[regionName], timestamp: null } }))
         setRegions((prevstate) => ({
             ...prevstate, [regionName]: {
                 ...prevstate[regionName],
@@ -103,7 +112,7 @@ export default function Home() {
     }
     const dailyResetTimer = (region, ievent, time) => {
         return (
-             <Countdown intervalDelay={1000}  key={region} onComplete={(e) => handleTimerComplete(region, ievent)} date={parseInt(time)} /> //60000 * 60 * 18} />
+            <Countdown intervalDelay={1000} key={region} onComplete={(e) => handleTimerComplete(region, ievent)} date={parseInt(time)} /> //60000 * 60 * 18} />
         )
     }
 
@@ -134,19 +143,48 @@ export default function Home() {
             resetTimer(target.name, target.dataset.ievent)
         }
     }
-
+    if (loading) return <></>
     return (
         <>
             <div className="container">
-                <h1 className="mt-2">PokeWatch | Lets go!</h1>
-                <br /><br />
+                <br />
                 <Accordion className="mb-3">
                     <Form.Group className="d-flex align-items-center sickbg">
                         <Accordion.Toggle size="md" className={regions.kanto.btnClassNames + " match-button-width ml-3"} data-ievent='0' eventKey={regions.kanto.event} name={REGIONNAMES.kanto} as={Button} onClick={handleClick}>{regions.kanto.button}</Accordion.Toggle>
                         <Form.Label className="bigText ml-3">Kanto: {regions.kanto.timer}</Form.Label>
                     </Form.Group>
                     <Accordion.Collapse eventKey='0'>
-                        <span>Cool pic here ; )</span>
+                        <Row>
+                            <Col><img src={kanto_clean} /></Col>
+                            <Col>
+                                <Table className="white-text h-100 w-100" striped bordered>
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            {Object.keys(kantoRoute).map((item, key) => {
+                                                return <th key={key}>{item}</th>
+                                            })}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {/* {console.log(kantoRoute.Route)} */}
+                                        {kantoRoute.Route.map((item, key) => {
+                                            return (
+                                                <tr key={key}>
+                                                    <th>{key + 1}</th>
+                                                    <th key={key}>
+                                                        {item}
+                                                    </th>
+                                                    <th key={key + 1}>
+                                                        {kantoRoute.Type[key]}
+                                                    </th>
+                                                </tr>
+                                            )
+                                        })}
+                                    </tbody>
+                                </Table>
+                            </Col>
+                        </Row>
                     </Accordion.Collapse>
                 </Accordion>
                 <Accordion className="mb-3">
